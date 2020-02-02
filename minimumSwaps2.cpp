0cc67844cc105e06334106562072275a708ed515
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-02-02 15:01:36 -0800

    Solved the minimumSwaps2 problem

--- minimumSwaps2.cpp
+++ minimumSwaps2.cpp
@@ -1,5 +1,25 @@
 // link: https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays&h_r=next-challenge&h_v=zen
 
+int swap(int &firstNum, int &secondNum) {
+	int temp= firstNum;
+	firstNum= secondNum;
+	secondNum= temp;
+}
+
 int minimumSwaps(vector<int> arr) {
-	
+	int size{arr.size()};
+	int minSwaps{0};
+
+	for (int i= 0; i < size; ++i) {
+		if (i + 1 != arr[i]) {
+			int j{i + 1};
+			while (arr[j] != i + 1)
+				++j;
+
+			swap(arr[i], arr[j]);
+			++minSwaps;
+		}
+	}
+
+	return minSwaps;
 }

2b604d4fb9e0557c33ae59472f3042d1b40c9644
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-02-02 14:24:01 -0800

    Not much progress has been made

---
+++ minimumSwaps2.cpp
@@ -0,0 +1,5 @@
+// link: https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays&h_r=next-challenge&h_v=zen
+
+int minimumSwaps(vector<int> arr) {
+	
+}

5fced15059e381b5aab72116e6f53e8951a3f845
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-02-29 02:11:21 -0800

    One test case left

--- CountTriplets.cpp
+++ CountTriplets.cpp
@@ -1,5 +1,6 @@
 // problem link: https://www.hackerrank.com/challenges/count-triplets-1/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=dictionaries-hashmaps&isFullScreen=true
 
+/*
 long countTriplets(vector<long> arr, long r) {
     long count{0};
 
@@ -29,31 +30,32 @@ long countTriplets(vector<long> arr, long r) {
 
     return count;
 }
+*/
 
 // second Solution
-int findClosestGreaterIndex(int index; vector<int> arr) {
-    for (int id= 0; id < vector.size(); ++id) {
-        if (index < vector[id])
-            return id;
-    }
-
-    return -1;
-}
 
 // Complete the countTriplets function below.
 long countTriplets(vector<long> arr, long r) {
     long count{0};
 
-    std::map<long, vector<int>> tripletsCollection;
+    std::map<long, vector<int>> backTracker;
+    std::map<long, long> counter;
 
     for (int index= 0; index < arr.size(); ++index) {
-        tripletsCollection[arr[index]].push_back(index);
-    }
+        if (backTracker.count(arr[index] / r) == 0) {
+            backTracker[arr[index]].push_back(0);
+            counter[arr[index]]= 0;
+        }
+        else {
+            int currentNum= backTracker[arr[index] / r].size();
+            if (backTracker.count(arr[index] / (r * r)) != 0) 
+                count+= counter[arr[index] / r];
 
-    for (auto aTriplet : tripletsCollection) { // aTriplet.second will be a vector
-        int firstNum= aTriplet.first;
-        for (int firstIndex : aTriplet.second) {
-            
+            backTracker[arr[index]].push_back(currentNum);
+            if (counter.count(arr[index]) == 0) {
+                counter[arr[index]]= 0 + currentNum;
+            }
+            else counter[arr[index]]+= currentNum;
         }
     }
 

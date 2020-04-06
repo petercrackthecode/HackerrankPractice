e6a156168eed1b7e23d11b5cee5b30d8b1592de0
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-04-05 23:36:46 -0700

    New problem

---
+++ FraudulentActivityNotifications.cpp
@@ -0,0 +1,6 @@
+// link: https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+int activityNotifications(vector<int> expenditure, int d) {
+
+
+}

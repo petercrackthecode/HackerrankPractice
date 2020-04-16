c2f0a8333ccf8b586b32647564221431f36b74e6
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-04-16 12:59:35 -0700

    Maybe we don't have to sort the array, but just to keep a track of it with count

--- FraudulentActivityNotifications.cpp
+++ FraudulentActivityNotifications.cpp
@@ -1,20 +1,43 @@
-// link: https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
-
 void putNumberIntoRightPos(vector<int> &expenditure, int startPos, int endPos) {
-    int subArrayEndPos= endPos - 1,
-        traversingIndex= startPos;
+    int traversingIndex= startPos;
     bool isSorted= false;
     while (traversingIndex < endPos && !isSorted) {
-        if (expenditure[traversingIndex])
+        if (expenditure[endPos] <= expenditure[traversingIndex])   {
+            int insertedElem= expenditure[endPos];
+            expenditure.erase(expenditure.begin() + endPos);
+            expenditure.insert(expenditure.begin() + traversingIndex, insertedElem);
+            isSorted= true;
+        }
 
         ++traversingIndex;
     }
 }
 
+void countingSort(std::vector<int> &expenditures, int startPos, int endPos) {
+    std::vector<int> count(201, 0);
+    
+    for (int index= startPos; index <= endPos; ++index) {
+        ++count[expenditures[index]];
+    }
+    
+    int countIndex{0},
+        expenditureIndex{startPos};
+    
+    while (expenditureIndex <= endPos) {
+        while (count[countIndex] > 0) {
+            expenditures[expenditureIndex]= countIndex;
+            ++expenditureIndex;
+            --count[countIndex];
+        }
+        ++countIndex;
+    }
+}
+
 double getMedium(std::vector<int> &expenditure, int startPos, int endPos) {
     int subArraySize{endPos - startPos + 1};
-    if (startPos == 0)
-        std::sort(expenditure.begin() + startPos, expenditure.begin() + endPos);
+    if (startPos == 0)  {
+        countingSort(expenditure, startPos, endPos);
+    }
     else putNumberIntoRightPos(expenditure, startPos, endPos);
 
     double medium= (subArraySize % 2 != 0) ? expenditure[subArraySize / 2 + startPos]
@@ -23,11 +46,66 @@ double getMedium(std::vector<int> &expenditure, int startPos, int endPos) {
     return medium;
 }
 
+int findSmallestExpenditure(std::vector<int> count) {
+    for (int index= 0; index <= 200; ++index) {
+        if (count[index] > 0) {
+            return index;
+        }
+    }
+
+    return 200;
+}
+
+int findClosestLargerIndex(std::vector<int> count, int index) {
+    for (int traversal= index + 1; traversal <= 200; ++traversal) {
+        if (count[traversal] > 0) {
+            return traversal;
+        }
+    }
+
+    return 200;
+}
+
+int findClosestSmallerIndex(std::vector<int> count; int index) {
+    for (int traversal= index - 1; traversal >= 0; --traversal) {
+        if (count[traversal] > 0) {
+            return traversal;
+        }
+    }
+}
+
+int findDoubledMediumExpenditure(std::vector<int> count, int days, bool areDaysEven) {
+    int daysTracker{0};
+    int index{0};
+
+    while (daysTracker < days / 2) {
+        if (count[index] != 0) {
+            --count[index];
+            ++daysTracker;
+        }
+        else ++index;
+    }
+
+    
+}
+
 // Complete the activityNotifications function below.
 int activityNotifications(std::vector<int> expenditure, int d) {
-    int notifications{0};
+    int notifications{0},
+        smallestExpenditure{0},
+        currentMedium= 0;
+    std::vector<int> count(201, 0);
+    bool areDaysEven= d % 2 == 0 ? true : false;
+
     for (int index= 0; index <= expenditure.size() - d; ++index) {
-        double medium= getMedium(expenditure, index, index + d - 1);
+        // double medium= getMedium(expenditure, index, index + d - 1);
+        if (index == 0) {
+            for (int i= index; i < index + d) {
+                ++count[i];
+            }
+            smallestExpenditure= findSmallestExpenditure(count);
+        }
+
         if (expenditure[index + d] >= medium * 2)
             ++notifications;
     }
